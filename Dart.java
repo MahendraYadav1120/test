03822b7f2620d129906c979df4f9c4aee5af1e17
MahendraYadav1120 <121162092+MahendraYadav1120@users.noreply.github.com>
2022-12-26 16:03:49 +0530

    Final commit

---
+++ Dart.java
@@ -0,0 +1,14 @@
+package pak2;
+import pak1.*;
+//import pak1.Pythan;
+public class Dart extends Pythan {
+    public void dis(){
+        System.out.println("its Dart");
+    }
+
+    public static void main(String[] args) {
+
+        Dart obj=new Dart();
+        obj.show();
+    }
+}
